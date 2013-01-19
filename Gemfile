bf0458ba8893acb4161b0c5c2012505775c24a80
dcollazo <davidcollazo@gmail.com>
2013-01-19 12:48:06 -0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+gem 'jquery-rails'
+
+gem 'bcrypt-ruby'
+gem 'bootstrap-sass'
+gem 'bootstrap-will_paginate', '0.0.6'
+gem 'faker'
+gem 'will_paginate'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails'
+  gem 'spork'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.5'
+  gem 'coffee-rails', '~> 3.2.2'
+  gem 'uglifier', '>= 1.2.3'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '728eaf9890e1c335e1b56fdb8c2a1d366ac292ddf6211fa3b1d77ba27de5a7f712393a68693b231e8a42b867cccd5171d59a85a3449fed4c3bb97bcf9ceb6d4f'
